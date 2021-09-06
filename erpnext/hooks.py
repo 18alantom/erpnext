00f1b9a4ce2ddf376135b826bb5b18e710488327
18alantom <2.alan.tom@gmail.com>
2021-09-06 19:55:39 +0530

    fix: add comma to hooks.py list before func

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -385,7 +385,7 @@ scheduler_events = {
 		"erpnext.healthcare.doctype.patient_appointment.patient_appointment.update_appointment_status",
 		"erpnext.buying.doctype.supplier_quotation.supplier_quotation.set_expired_status",
 		"erpnext.accounts.doctype.process_statement_of_accounts.process_statement_of_accounts.send_auto_email",
-		"erpnext.non_profit.doctype.membership.membership.set_expired_status"
+		"erpnext.non_profit.doctype.membership.membership.set_expired_status",
 		"erpnext.quality_management.doctype.quality_goal.quality_goal.create_scheduled_quality_reviews"
 	],
 	"daily_long": [
